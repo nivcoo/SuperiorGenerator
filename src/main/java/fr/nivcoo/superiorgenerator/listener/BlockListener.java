0c4e026d3d3cd9f29c132dd6f62e4fcb34f80bfb
nivcoo <nico64800@live.fr>
2022-01-14 18:24:17 +0100

    improv. maybe works with all version

--- src/main/java/fr/nivcoo/superiorgenerator/listener/BlockListener.java
+++ src/main/java/fr/nivcoo/superiorgenerator/listener/BlockListener.java
@@ -6,6 +6,7 @@ import fr.nivcoo.superiorgenerator.hook.SuperiorSkyblock2;
 import fr.nivcoo.superiorgenerator.manager.Generator;
 import fr.nivcoo.superiorgenerator.manager.GeneratorManager;
 import fr.nivcoo.utilsz.config.Config;
+import fr.nivcoo.utilsz.version.ServerVersion;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
@@ -54,13 +55,13 @@ public class BlockListener implements Listener {
             return;
 
         boolean enableBasaltGen = config.getBoolean("enable_basalt_generator");
-        if (event.getNewState().getType() == Material.LAVA) {
+        if ((ServerVersion.isServerVersionAtLeast(ServerVersion.V1_13) && event.getNewState().getType() == Material.getMaterial("LAVA")) || event.getNewState().getType() == Material.getMaterial("STATIONARY_LAVA")) {
             event.setCancelled(true);
             Block relBlock = event.getBlock().getRelative(BlockFace.DOWN);
-            if (relBlock.getType() == Material.WATER)
+            if ((ServerVersion.isServerVersionAtLeast(ServerVersion.V1_13) && relBlock.getType() == Material.getMaterial("WATER")) || relBlock.getType() == Material.getMaterial("STATIONARY_WATER"))
                 generateRandomBlock(relBlock.getState(), islandUUID);
         } else if (event.getNewState().getType() == Material.COBBLESTONE ||
-                (event.getNewState().getType() == Material.getMaterial("BASALT") && enableBasaltGen))
+                (ServerVersion.isServerVersionAtLeast(ServerVersion.V1_16) && event.getNewState().getType() == Material.getMaterial("BASALT") && enableBasaltGen))
             generateRandomBlock(event.getNewState(), islandUUID);
 
 
