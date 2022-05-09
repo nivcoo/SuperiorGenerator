1c4b41a951b6da40303f9b469e6a1b1b35b912c7
nivcoo <nico64800@live.fr>
2022-05-09 17:41:42 +0200

    improv. fix folders and version

--- API/src/main/java/fr/nivcoo/superiorgeneratorapi/ASuperiorGenerator.java
+++ API/src/main/java/fr/nivcoo/superiorgeneratorapi/ASuperiorGenerator.java
@@ -3,6 +3,5 @@ package fr.nivcoo.superiorgeneratorapi;
 import fr.nivcoo.superiorgeneratorapi.manager.AGeneratorManager;
 
 public interface ASuperiorGenerator {
-
     AGeneratorManager getGeneratorManager();
 }
